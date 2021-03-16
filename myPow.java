c4ecbf7955ff8e43c0a6c5c03ab7cb8b61bd0e19
Amrinder singh <amrinders433@gmail.com>
2021-03-15 21:31:50 -0400

    Create myPow.java

---
+++ myPow.java
@@ -0,0 +1,26 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+};
+
